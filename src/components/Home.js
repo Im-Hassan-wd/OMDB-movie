fd7c5a4be62438f2388e03425bb1fba4116d11c9
Im-Hassan-wd <alhassansahih@gmail.com>
2022-03-24 20:16:35 +0100

    search

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,7 @@
-import { useEffect, useState } from "react";
 import Banner from "./Banner";
-import Footer from "./Footer";
-import Movie from "./Movie";
-import Navbar from "./Navbar";
 import Search from "./Search";
-import { useFetch } from '../hook/useFetch';
-
-const movieTitles =  [
-    'avengers',
-    'inception',
-]
 
 const Home = () => {
-    const {error, isPending, data: movies } = useFetch('http://www.omdbapi.com/?s=avengers&apikey=474a7c0d')
-
-
-
     return (
         <div className="home">
             <Banner />
